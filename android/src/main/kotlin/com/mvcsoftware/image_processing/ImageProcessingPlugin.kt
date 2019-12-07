9c6c9355bba4129972686a02e88c202e0d802263
vmetal123 <bladi22@yahoo.com>
2019-12-06 23:27:15 -0600

    first commit

---
+++ android/src/main/kotlin/com/mvcsoftware/image_processing/ImageProcessingPlugin.kt
@@ -0,0 +1,52 @@
+package com.mvcsoftware.image_processing
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.util.DisplayMetrics
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry.Registrar
+import java.io.ByteArrayOutputStream
+
+class ImageProcessingPlugin: MethodCallHandler {
+  companion object {
+    @JvmStatic
+    fun registerWith(registrar: Registrar) {
+      val channel = MethodChannel(registrar.messenger(), "image_processing")
+      channel.setMethodCallHandler(ImageProcessingPlugin())
+    }
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
+    }
+
+    if(call.method == "combineImages") {
+      val firstImage: ByteArray? = call.argument("firstImage")
+      val secondImage: ByteArray? = call.argument("secondImage")
+      result.success(combineImages(firstImage, secondImage))
+    }
+  }
+
+  private fun combineImages(firstImage: ByteArray?, secondImage: ByteArray?): ByteArray? {
+    val firstBitmap: Bitmap = BitmapFactory.decodeByteArray(firstImage, 0, firstImage!!.size)
+    val secondBitmap: Bitmap = BitmapFactory.decodeByteArray(secondImage, 0, secondImage!!.size)
+
+    val result: Bitmap = Bitmap.createBitmap(firstBitmap.width, firstBitmap.height, firstBitmap.config)
+
+    var canvas: Canvas = Canvas(result)
+
+    canvas.drawBitmap(firstBitmap, 0f, 0f, null)
+    canvas.drawBitmap(secondBitmap, 10f, 10f, null)
+
+    var resultBiteArray: ByteArrayOutputStream = ByteArrayOutputStream()
+    result.compress(Bitmap.CompressFormat.PNG, 100, resultBiteArray)
+    return resultBiteArray.toByteArray()
+  }
+}
---
+++ lib/image_processing.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+import 'dart:typed_data';
+
+import 'package:flutter/services.dart';
+
+class ImageProcessing {
+  static const MethodChannel _channel =
+      const MethodChannel('image_processing');
+
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+  
+  static Future<Uint8List> CombineImages(Uint8List firstImage, Uint8List secondImage) async {
+    final imageResult = await _channel.invokeMethod('combineImages', <String, dynamic> {
+      'firstImage': firstImage,
+      'secondImage': secondImage
+    });
+    return imageResult;
+  }
+}
